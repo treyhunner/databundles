d073b0951918b4dfa22ec45bce6cecd00da6c14f
Eric Busboom <eric@clarinova.com>
2012-06-10 06:55:29 -0700

    Moving files around

--- src/databundles/bundle.py
+++ src/databundles/bundle.py
@@ -15,17 +15,33 @@ class Bundle(object):
         '''
         Constructor
         '''
-        self.config = None
-        
+       
         if not directory:
-            root_dir = files.root_dir
+            root_dir = files.root_dir()
         else:
             root_dir = files.RootDir(directory)
-            
+
         self.root_dir = root_dir
-            
-        self.config = root_dir.bundle_config.yaml
-        
+               
     @property
     def config(self):
         return self.root_dir.bundle_config
+
+    def prepare(self):
+        pass
+    
+    def download(self):
+        pass  
+    
+    def transform(self):
+        pass
+    
+    def build(self):
+        pass
+    
+    def submit(self):
+        pass
+    
+    
+    
+    
