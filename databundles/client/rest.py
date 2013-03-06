3f600a8098b886f9bdfc3274ca1ad12eb54e2457
Eric Busboom <eric@clarinova.com>
2013-03-06 10:36:13 -0800

    ?

--- databundles/client/rest.py
+++ databundles/client/rest.py
@@ -205,12 +205,14 @@ class Rest(object):
         from collections import namedtuple
         Ref1= namedtuple('Ref1','Dataset Partition')
         Ref2= namedtuple('Ref2','Dataset')
-        EntryT = namedtuple('Entry','id name source dataset subset variation creator revision')
+        EntryT = namedtuple('Entry','id name source dataset subset variation creator revision time space table grain')
         
         # Allow constructing Entry with missing values
         def Entry(id=None,name=None,source=None,dataset=None,subset=None,
-                  variation=None,creator=None,revision=None,):
-            return EntryT(id, name, source, dataset, subset, variation, creator, revision)
+                  variation=None,creator=None,revision=None,
+                  time=None, space=None, table=None, grain=None):
+            return EntryT(id, name, source, dataset, subset, variation, creator, revision,
+                          time,space,table,grain)
 
         return [ (Ref1(Entry(**i['dataset']) ,Entry(**i['partition'])) if i['partition'] 
                 else  Ref2(Entry(**i['dataset']))) for i in r  if i is not False]
