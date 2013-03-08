b569da4fb662e0a28a27bc451273660e04d7eebb
Eric Busboom <eric@clarinova.com>
2013-03-07 20:46:13 -0800

    Trying to get install.sh working

--- install.sh
+++ install.sh
@@ -25,28 +25,25 @@ if [ -z "$install_dir" ]; then
     exit 1
 fi
 
-echo "Installing base packages. May need to ask for root password"
+echo "--- Installing base packages. May need to ask for root password"
 command -v apt-get >/dev/null 2>&1; has_aptget=$?
 command -v brew >/dev/null 2>&1; has_brew=$?
 
 if [ $has_aptget -eq 0 ]; then
-    echo "Installing base packages with apt-get"
+    echo "--- Installing base packages with apt-get"
     sudo apt-get install -y gdal-bin sqlite3 spatialite-bin curl git 
-    sudo apt-get install -y python-gdal # python-h5py python-numpy python-scipy
+    sudo apt-get install -y python-gdal python-h5py python-numpy python-scipy
     sudo apt-get install -y libpq-dev libhdf5-dev hdf5-tools h5utils 
 elif [ `uname` = 'Darwin' ]; then
     if [ $has_brew -eq 0 ]; then
-        echo "Installing with Homebrew"
+        echo "--- Installing with Homebrew"
         # Install homebrew with:
         #ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
         brew install git
         brew install gdal
         brew install hdf5
         brew install spatialite-tools
-        
-        echo "For Macs using brew"
-        echo "ERROR: Mac support not Finished!"
-        exit 1
+
     else
         echo "ERROR: For Macs, but could not find a package manager. "
         exit 1
@@ -62,6 +59,7 @@ if [ ! -d $install_dir ]; then
 fi
 
 # Basic virtualenv setup.
+echo "--- Building virtualenv"
 mkdir -p $install_dir/bin
 curl https://raw.github.com/pypa/virtualenv/master/virtualenv.py > $install_dir/bin/virtualenv.py
 /usr/bin/python $install_dir/bin/virtualenv.py --system-site-packages $install_dir
@@ -69,11 +67,9 @@ curl https://raw.github.com/pypa/virtualenv/master/virtualenv.py > $install_dir/
 # Source the activate script to get it going
 . $install_dir/bin/activate
  
+echo "--- Install the databundles package from github"
 # Download the data bundles with pip so the code gets installed. 
 pip install -e 'git+https://github.com/clarinova/databundles#egg=databundles'
-
-# Arg! h5py depends on numpy, and pip can't deal with dependencies. 
-pip install numpy
 
 # Install the basic required packages
 pip install -r $install_dir/src/databundles/requirements.txt
@@ -82,6 +78,15 @@ if [ $? -ne 0 ]; then
     echo "ERROR: Requirements.txt installation failed!"
     exit 1
 fi
+
+#
+# These packages don't install propertly in the virtualenv in  Ubunty, so we
+# install them at the start via apt-get, but they install OK on Mac OS X.
+if [ `uname` = 'Darwin' ]; then
+    pip install gdal
+    pip install numpy
+    pip install h5py
+fi 
 
 # The actual bundles don't need to be installed
 git clone https://github.com/clarinova/civicdata $install_dir/src/civicdata
